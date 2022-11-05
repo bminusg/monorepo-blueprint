ecb685336b31051375fb2a4aad6b91be0bf474ab
Benny Gebauer <hey@bminusg,de>
2022-11-05 11:57:13 +0100

    Init commit

---
+++ backend/static/assets/AboutView.d4a8774c.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as o,a as s}from"./index.9c0dca1d.js";const _={},c={class:"about"},n=s("h1",null,"This is an About page",-1),a=[n];function i(r,u){return t(),o("div",c,a)}const l=e(_,[["render",i]]);export{l as default};
